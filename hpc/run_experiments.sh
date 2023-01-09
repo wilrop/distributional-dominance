760c893435b9ae18e174f7e959e1d79b64bee0c8
Willem Röpke <willem.ropke@vub.be>
2023-01-09 20:17:31 +0100

    Training script for on the HPC

---
+++ hpc/run_experiments.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=dist-dom
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=64gb
+#SBATCH --mail-user=willem.ropke@vub.be
+#SBATCH --mail-type=ALL
+#SBATCH --output=logs/output-%A.out
+#SBATCH --error=logs/err-%A.err
+
+# Load the necessary modules.
+module load Python/3.10.4-GCCcore-11.3.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load scikit-learn/1.1.2-foss-2022a
+pip install --user gym
+pip install --user mo-gym
+pip install --user ramo
+pip install --user pymoo
+pip install --user scikit-learn
+pip install --user POT
+pip install --user pulp
+
+# Navigate to the job directory.
+cd $VSC_HOME/distributional-dominance
+
+# Run the experiments.
+python3 experiments.py --log-dir $VSC_SCRATCH/results --save --log-every 5000
