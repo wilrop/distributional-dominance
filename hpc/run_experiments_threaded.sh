64d6da0f3683dcfb6d42b7537437e063fbc4e633
Willem Röpke <willem.ropke@vub.be>
2023-01-10 11:51:58 +0100

    Add jobscript for multithreaded execution

---
+++ hpc/run_experiments_threaded.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=dist-dom
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64gb
+#SBATCH --mail-user=willem.ropke@vub.be
+#SBATCH --mail-type=ALL
+#SBATCH --output=logs/output-%A.out
+#SBATCH --error=logs/err-%A.err
+
+# Load the necessary modules.
+module load Python/3.10.4-GCCcore-11.3.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load scikit-learn/1.1.2-foss-2022a
+pip install --user gym
+pip install --user mo-gym
+pip install --user ramo
+pip install --user pymoo
+pip install --user scikit-learn
+pip install --user POT
+pip install --user pulp
+
+# Define the log directory.
+LOGDIR="${VSC_SCRATCH}/results"
+
+# Run the experiments.
+python3 $VSC_HOME/distributional-dominance/experiments.py --log-dir "$LOGDIR" --save --log-every 5000 --num-threads -1
